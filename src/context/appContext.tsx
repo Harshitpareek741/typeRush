e17dcaf9789f84d896e27e92df66cdcd03497c85
Harshitpareek741 <b221190@skit.ac.in>
2025-01-12 10:58:59 +0530

    yes

--- src/context/appContext.tsx
+++ src/context/appContext.tsx
@@ -2,14 +2,19 @@ import React, { createContext, useMemo, useState } from "react";
 
 export const AppContext = createContext({
     wpm: 0,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setWpm: (wpm: number) => {},
     cpm: 0,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setCpm: (cpm: number) => {},
     accuracy: 0,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setAccuracy: (accuracy: number) => {},
     timer: 60,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setTimer: (timer: number | ((prevTimer: number) => number)) => {},
     username: "", // Default empty string
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setUsername: (username: string) => {}, // Default empty setter
 });
 
